12eb4088378e1b47f04cc1c31992be04c2a18c82
Mohammad Awad <Mohammed.awad@god.de>
2024-05-18 19:47:14 +0200

    Auto-generated commit

--- chunk-SZ4QS6QR.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-AZTOTPBV.js";import"./chunk-XXSXTUOG.js";import"./chunk-6F6LHVFX.js";export{a as MainBodyComponent};
---
+++ chunk-ZHUXNQ4C.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-XZVIAAYL.js";import"./chunk-XXSXTUOG.js";import"./chunk-6F6LHVFX.js";export{a as MainBodyComponent};
