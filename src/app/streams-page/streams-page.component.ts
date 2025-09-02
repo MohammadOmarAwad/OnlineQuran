f1cbec33e3e8bf1fe54cde4cd8a469e2254a7100
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2025-09-02 13:59:11 +0200

    Streaming

--- src/app/streams-page/streams-page.component.ts
+++ src/app/streams-page/streams-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation, inject } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-streams-page',
@@ -7,4 +8,31 @@ import { Component, ViewEncapsulation, inject } from '@angular/core';
   styleUrl: './streams-page.component.css',
   encapsulation: ViewEncapsulation.None
 })
-export class StreamsPageComponent {} 
+export class StreamsPageComponent {
+
+  ComponentLiveMakka: SafeHtml;
+  ComponentLiveMadina: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.Run_Audio('https://www.youtube.com/embed/fRLNpcIwz34', 'Makka');
+    this.Run_Audio('https://www.youtube.com/embed/TpT8b8JFZ6E', 'Madina');
+  }
+
+
+  Run_Audio(url: string, Element: String): void {
+    const iframeString = `
+      <iframe class='DivBox' width="560" height="315"
+        src='${url}'
+        frameborder="0"
+        allowfullscreen>
+      </iframe>`;
+
+    if (Element == 'Makka') {
+      this.ComponentLiveMakka = this.sanitizer.bypassSecurityTrustHtml(iframeString);
+    }
+
+    if (Element == 'Madina') {
+      this.ComponentLiveMadina = this.sanitizer.bypassSecurityTrustHtml(iframeString);
+    }
+  }
+} 
