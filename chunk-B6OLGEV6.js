f9667c003957fb0736cd1d4bb7bfa6a343230564
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2025-09-08 08:56:53 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/MohammadOmarAwad/OnlineQuran/commit/e4a19fd40b9ba104c674c58fba3bbec630ea7b9e

---
+++ chunk-B6OLGEV6.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-6SXQG7E6.js";import"./chunk-62KA3V5N.js";import"./chunk-DQ6BPTAM.js";import"./chunk-6N6ZIER7.js";export{a as MainBodyComponent};
--- chunk-IIXZ6UQQ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-5V6W2MUG.js";import"./chunk-62KA3V5N.js";import"./chunk-DQ6BPTAM.js";import"./chunk-6N6ZIER7.js";export{a as MainBodyComponent};
