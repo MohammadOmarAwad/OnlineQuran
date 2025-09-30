5b8f9dba3eae8115db1e8fb77040de5bfbfa21f8
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2025-09-30 19:04:10 +0200

    Auto-generated commit

--- chunk-MYSUQGVY.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-NLMCGKG6.js";import"./chunk-URFG6QR7.js";import"./chunk-GGE53DXE.js";import"./chunk-H4KROINN.js";export{a as MainBodyComponent};
---
+++ chunk-QQP3MVCV.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-IBS2EEGY.js";import"./chunk-URFG6QR7.js";import"./chunk-GGE53DXE.js";import"./chunk-H4KROINN.js";export{a as MainBodyComponent};
