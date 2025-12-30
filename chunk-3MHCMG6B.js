3462985d22f5305d1c903b3630d41ce39c5d4858
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2025-12-30 16:05:26 +0000

    moment-hijri
    Triggered by commit: https://github.com/MohammadOmarAwad/OnlineQuran/commit/c38982bf0dbc8a6fb28a513caaee6f825f60975b

--- chunk-3MHCMG6B.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-TCC4ZY3L.js";import"./chunk-7ASGHYWX.js";import"./chunk-BYJ3UFST.js";import"./chunk-SYQUMVXP.js";export{a as MainBodyComponent};
---
+++ chunk-ELXAB6UK.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-IY4O6PE2.js";import"./chunk-63VDVSM5.js";import"./chunk-XTRDQCKI.js";import"./chunk-52ZRRINH.js";export{a as MainBodyComponent};
