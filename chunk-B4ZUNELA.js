6fa1f8d748d47a8060cbf433be6680a18565d860
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2026-01-02 06:01:25 +0000

    Style in Search
    Triggered by commit: https://github.com/MohammadOmarAwad/OnlineQuran/commit/492f71eb91d01cd9fe2bc01bfc8ba6480304262a

---
+++ chunk-B4ZUNELA.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-TNQ6PJMP.js";import"./chunk-AQ732WLV.js";import"./chunk-ZDNZXBSK.js";import"./chunk-Z7MV2M5O.js";export{a as MainBodyComponent};
--- chunk-IHXC3MNA.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-YNFO2EPF.js";import"./chunk-AQ732WLV.js";import"./chunk-ZDNZXBSK.js";import"./chunk-Z7MV2M5O.js";export{a as MainBodyComponent};
