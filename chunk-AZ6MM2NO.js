e75ab6c341c0ed2c9e5cd4d1802e785be2e5d58b
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2026-01-04 14:44:45 +0000

    Add Async Await
    Triggered by commit: https://github.com/MohammadOmarAwad/OnlineQuran/commit/94f78c736242dea536e141b9e56e29672378cd2c

---
+++ chunk-AZ6MM2NO.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WQV3ESDV.js";import"./chunk-6OQD6EOR.js";import"./chunk-ZUYYOHZW.js";import"./chunk-6PEMDKXP.js";import"./chunk-Z7MV2M5O.js";export{a as MainBodyComponent};
--- chunk-B4ZUNELA.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-TNQ6PJMP.js";import"./chunk-AQ732WLV.js";import"./chunk-ZDNZXBSK.js";import"./chunk-Z7MV2M5O.js";export{a as MainBodyComponent};
