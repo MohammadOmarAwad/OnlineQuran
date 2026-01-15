47ac932c63d103bfe806bbb75d0a9beb86fc43f1
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2026-01-15 08:10:27 +0000

    add More @media
    Triggered by commit: https://github.com/MohammadOmarAwad/OnlineQuran/commit/2664fffde92d2f6fa23c7987796bea6bb8d0d1e2

--- chunk-EDA2ZTLD.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-ZPRO5ILM.js";import"./chunk-NNUGPJHL.js";import"./chunk-4SKFXSQS.js";import"./chunk-AKDFF4B2.js";import"./chunk-7FJHUUWP.js";export{a as MainBodyComponent};
---
+++ chunk-RONCEMBC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WZCNVB4Y.js";import"./chunk-NNUGPJHL.js";import"./chunk-4SKFXSQS.js";import"./chunk-AKDFF4B2.js";import"./chunk-7FJHUUWP.js";export{a as MainBodyComponent};
