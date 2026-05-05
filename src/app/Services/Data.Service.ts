34a77569b52a679964ea84dca709522aa4463531
Mohammed.awad <mohammad.omar.awad@gmail.com>
2026-05-05 14:26:49 +0200

    Refactoring

--- src/app/Services/Data.Service.ts
+++ src/app/Services/Data.Service.ts
@@ -8,6 +8,37 @@ export class DataService {
     private static surahsCache: SurahModel[] | null = null;
     private static ayahsCache: AyahModel[] | null = null;
 
+    //Get List of SurahList
+    static async GetSurahsData(): Promise<SurahModel[]> {
+        if (this.surahsCache) return this.surahsCache;
+
+        const csv = await this.GetData(UrlResource.SurahsList_CSV_Url);
+        const parsed = this.parseCsv<SurahModel>(csv);
+
+        this.surahsCache = parsed.data;
+        return this.surahsCache;
+    }
+
+    //Get List of AyahList
+    static async GetAyasData(): Promise<AyahModel[]> {
+        if (this.ayahsCache) return this.ayahsCache;
+
+        const csv = await this.GetData(UrlResource.AyahsList_CSV_Url);
+        const parsed = this.parseCsv<AyahModel>(csv);
+
+        this.ayahsCache = parsed.data;
+        return this.ayahsCache;
+    }
+
+    //Parse the CSV Rows
+    private static parseCsv<T>(csv: string) {
+        return Papa.parse<T>(csv, {
+            header: true,
+            skipEmptyLines: true,
+            dynamicTyping: true
+        });
+    }
+
     // Get Data as Text
     private static async GetData(url: string): Promise<string> {
         const response = await fetch(url);
@@ -17,38 +48,6 @@ export class DataService {
         }
 
         return await response.text(); // ✅ correct
-    }
-
-    //Get List of Surah
-    static async GetSurahsData(): Promise<SurahModel[]> {
-        if (this.surahsCache) return this.surahsCache;
-
-        const csv = await this.GetData(UrlResource.SurahsList_CSV_Url);
-
-        const parsed = Papa.parse<SurahModel>(csv, {
-            header: true,
-            skipEmptyLines: true,
-            dynamicTyping: true
-        });
-
-        this.surahsCache = parsed.data;
-        return this.surahsCache;
-    }
-
-    //Get List of Surah
-    static async GetAyasData(): Promise<AyahModel[]> {
-        if (this.ayahsCache) return this.ayahsCache;
-
-        const csv = await this.GetData(UrlResource.AyahsList_CSV_Url);
-
-        const parsed = Papa.parse<AyahModel>(csv, {
-            header: true,
-            skipEmptyLines: true,
-            dynamicTyping: true
-        });
-
-        this.ayahsCache = parsed.data;
-        return this.ayahsCache;
     }
 
 }
