75ac7d98039e39134acfcf45f72d9a9cf140a879
Mohammad Awad <Mohammad.omar.awad@gmail.com>
2026-09-16 08:30:18 +0000

    Add Run Audio to MenuList
    Triggered by commit: https://github.com/MohammadOmarAwad/OnlineQuran/commit/6b1f2a077d6d129e7a6665f5f723f331db837037

--- chunk-JQYDJM6B.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-DSD75NQO.js";import"./chunk-A5ZPSPIA.js";import"./chunk-ECBWW326.js";import"./chunk-E3F45BML.js";import"./chunk-7FJHUUWP.js";export{a as MainBodyComponent};
---
+++ chunk-N7QAXK6D.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-O5ZGI6KH.js";import"./chunk-SCFNK4C3.js";import"./chunk-72SGZVFQ.js";import"./chunk-UOJF66M6.js";import"./chunk-ZT2SJYAS.js";export{a as MainBodyComponent};
